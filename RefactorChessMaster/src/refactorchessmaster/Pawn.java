c4b025d8946f2de75ea8a40332761beae230aa18
Fernando_Machaca <fernando.machaca@unmsm.edu.pe>
2023-06-08 13:14:49 -0500

    se utilizo la tecnica "extract method" al calculatePossibleMoves de pawn.java

--- RefactorChessMaster/src/refactorchessmaster/Pawn.java
+++ RefactorChessMaster/src/refactorchessmaster/Pawn.java
@@ -69,53 +69,48 @@ public class Pawn
      * @return ArrayList<String> the moves
      */
     @Override
-    protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
+    protected ArrayList<String> calculatePossibleMoves(ChessGameBoard board) {
         ArrayList<String> moves = new ArrayList<String>();
-        if ( isPieceOnScreen() ){
-            int currRow =
-                getColorOfPiece() == ChessGamePiece.WHITE
-                    ? ( pieceRow - 1 )
-                    : ( pieceRow + 1 );
+        if (isPieceOnScreen()) {
+            int currRow
+                    = getColorOfPiece() == ChessGamePiece.WHITE
+                            ? (pieceRow - 1)
+                            : (pieceRow + 1);
             int count = 1;
             int maxIter = notMoved ? 2 : 1;
             // check for normal moves
-            while ( count <= maxIter ){ // only loop while we have open slots and have not passed our
-              // limit
-                if ( isOnScreen( currRow, pieceColumn )
-                    && board.getCell( currRow,
-                        pieceColumn ).getPieceOnSquare() == null ){
-                    moves.add( currRow + "," + pieceColumn );
-                }
-                else
-                {
+            while (count <= maxIter) { // only loop while we have open slots and have not passed our
+                // limit
+                if (isOnScreen(currRow, pieceColumn)
+                        && board.getCell(currRow,
+                                pieceColumn).getPieceOnSquare() == null) {
+                    moves.add(currRow + "," + pieceColumn);
+                } else {
                     break;
                 }
-                currRow =
-                    ( getColorOfPiece() == ChessGamePiece.WHITE )
-                        ? ( currRow - 1 )
-                        : ( currRow + 1 );
+                currRow
+                        = (getColorOfPiece() == ChessGamePiece.WHITE)
+                                ? (currRow - 1)
+                                : (currRow + 1);
                 count++;
             }
             // check for enemy capture points
-            if ( getColorOfPiece() == ChessGamePiece.WHITE ){
-                if ( isEnemy( board, pieceRow - 1, pieceColumn - 1 ) ){
-                    moves.add( ( pieceRow - 1 ) + "," + ( pieceColumn - 1 ) );
-                }
-                if ( isEnemy( board, pieceRow - 1, pieceColumn + 1 ) ){
-                    moves.add( ( pieceRow - 1 ) + "," + ( pieceColumn + 1 ) );
-                }
-            }
-            else
-            {
-                if ( isEnemy( board, pieceRow + 1, pieceColumn - 1 ) ){
-                    moves.add( ( pieceRow + 1 ) + "," + ( pieceColumn - 1 ) );
-                }
-                if ( isEnemy( board, pieceRow + 1, pieceColumn + 1 ) ){
-                    moves.add( ( pieceRow + 1 ) + "," + ( pieceColumn + 1 ) );
-                }
+            if (getColorOfPiece() == ChessGamePiece.WHITE) {
+                addEnemyCaptureMove(board, moves, pieceRow - 1, pieceColumn - 1);
+                addEnemyCaptureMove(board, moves, pieceRow - 1, pieceColumn + 1);
+            } else {
+                addEnemyCaptureMove(board, moves, pieceRow + 1, pieceColumn - 1);
+                addEnemyCaptureMove(board, moves, pieceRow + 1, pieceColumn + 1);
             }
         }
         return moves;
+    }
+
+    private void addEnemyCaptureMove(
+            ChessGameBoard board, ArrayList<String> moves, int row, int column) {
+        if (isEnemy(board, row, column)) {
+            moves.add(row + "," + column);
+        }
     }
     /**
      * Creates an icon for this piece depending on the piece's color.
